80ae93ab26c766a6dd1b5095b5daad7f3bfa215a
jessica_conejo <jessica.vcm1902@gmail.com>
2023-01-08 11:26:50 -1100

    cambio

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    state = {  } 
+    render() { 
+        return (
+
+         <div className='footer'>   <span className='mt-5'>©️ Jessica CM 2023</span></div>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+
+class Navbar extends Component {
+    state = {  } 
+    render() { 
+        return (
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">HOME <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">WORKS</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">CONTACT ME</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        );
+    }
+}
+ 
+export default Navbar;
